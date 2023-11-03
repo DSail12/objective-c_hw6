d4eb361fd63896497769fdc09fd09f2432d44e06
dsail <dsail@yandex.ru>
2023-11-03 14:29:05 +0300

    commit

---
+++ objective-c_hw6/ColorTile.m
@@ -0,0 +1,12 @@
+//
+//  ColorTile.m
+//  objective-c_hw6
+//
+//  Created by Eugene Zvyagin on 03.11.2023.
+//
+
+#import "ColorTile.h"
+
+@implementation ColorTile
+
+@end
---
+++ objective-c_hw6/Robot.h
@@ -0,0 +1,16 @@
+//
+//  Robot.h
+//  objective-c_hw6
+//
+//  Created by Eugene Zvyagin on 03.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
